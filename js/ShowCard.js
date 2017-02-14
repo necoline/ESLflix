89f3bfdd9c454ec940cdfb759331aacfee4e24f6
necoline <hnhubner@gmail.com>
2017-02-14 15:13:47 -0700

    first commit

---
+++ js/ShowCard.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link }  from 'react-router'
+const { string } = React.PropTypes
+//shape = obj that is described
+
+const ShowCard = React.createClass({
+  propTypes: {
+      poster: string.isRequired,
+      title: string.isRequired,
+      year: string.isRequired,
+      description: string.isRequired,
+      imdbID: string.isRequired
+  },
+  render() {
+    const { poster, title, year, description, imdbID } = this.props
+    return (
+      <Link to={`/details/${this.props.imdbID}`}>
+        <div  className='show-card'>
+          <img src={`/public/img/posters/${poster}`} />
+          <div>
+            <h3>{title}</h3>
+            <h4>{year}</h4>
+            <p>{description}</p>
+          </div>
+        </div>
+      </Link>
+    )
+  }
+})
+
+export default ShowCard
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  context: __dirname,
+  entry: './js/ClientApp.js',
+  // devtool: 'cheap-module-source-map',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  devServer: {
+    publicPath: '/public/',
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat'
+    }
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        include: [
+          path.resolve(__dirname, 'js'),
+          path.resolve('node_modules/preact-compat/src')
+        ],
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
